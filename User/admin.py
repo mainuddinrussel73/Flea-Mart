ab1152cc38f7db643e64b5beddc529c74c89c871
mainuddinrussel73 <mainuddinrusselbuet73@gmail.com>
2017-11-23 00:41:01 +0600

    commentadded

--- User/admin.py
+++ User/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from User.models import SellItemInfo,Chat,Notification
+from User.models import SellItemInfo,Chat,Notification,Comments
 
 # Register your models here.
 
 admin.site.register(SellItemInfo)
 admin.site.register(Chat)
 admin.site.register(Notification)
+admin.site.register(Comments)
